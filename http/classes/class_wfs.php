e2856d5dd487e3d85795ca3d3de1ebd8517a6054
lvgl-cs <c.sommer@lvgl.saarland.de>
2024-04-30 14:57:49 +0200

    Ticket #7322: Change of atom feed wfs tile division logic to fallback to url parameters if geometrie elements(used in fes-filters) were not adequately registered for complex wfs

--- http/classes/class_wfs.php
+++ http/classes/class_wfs.php
@@ -814,7 +814,15 @@ $bboxFilter = '<fes:Filter xmlns:fes="http://www.opengis.net/fes/2.0"><fes:BBOX>
     			$url = $this->getFeature.$this->getConjunctionCharacter($this->getFeature)."service=WFS&request=GetFeature&version=".$version."&".strtolower($typeNameParameterName)."=".$featureTypeName."&resultType=hits".$namespaces;
     			
 				if ($filter != null) {
-    			    $url .= "&FILTER=".urlencode($filter);
+					//Ticket: 7322 - Due to registration issues with complex wfs services
+					//an additional filter logic was introduced in atom feed clients
+					//Conventional fes/ogc-Filters are replaced with BBOX + EPSG parameters int this case
+					//and must be concatenated differently here
+					if (!strpos($filter,"fes") && !strpos($filter,"ogc")){
+						$url .= $filter;
+					}else{
+						$url .= "&FILTER=".urlencode($filter);
+					}
     			}
 				#$e = new mb_exception("URL for count by GET: " . $url);
     			//auth is already integrated in ows class
