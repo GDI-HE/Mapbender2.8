6a325344caf29e3526091887cefd7432446907a8
dpakprajul <bindaaspratiks@gmail.com>
2026-04-13 14:38:03 +0200

    #85 SVG space managed by adding margin

--- http/plugins/mb_print.php
+++ http/plugins/mb_print.php
@@ -811,9 +811,9 @@ var PrintPDF = function (options) {
       var $progressWrap = $("[id='pfi-progress-wrap']");
       var $progressLabel = $("[id='pfi-progress-label']");
       $progressLabel.html(
-        '<?php echo _mb("PDF fertig:"); ?> <a href="' + pdfUrl + '" target="_blank" ' +
-        'style="font-weight:bold;color:#1a5fa8;text-decoration:none;display:inline-flex;align-items:center;gap:5px;">' +
-        '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 16 16" fill="currentColor" style="flex-shrink:0;">' +
+        '<span><?php echo _mb("PDF fertig:"); ?></span> <a href="' + pdfUrl + '" target="_blank" ' +
+        'style="font-weight:bold;color:#1a5fa8;text-decoration:none;">' +
+        '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 16 16" fill="currentColor" style="margin-bottom:-3px;margin-right:3px;">' +
           '<path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>' +
           '<path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>' +
         '</svg>' +
